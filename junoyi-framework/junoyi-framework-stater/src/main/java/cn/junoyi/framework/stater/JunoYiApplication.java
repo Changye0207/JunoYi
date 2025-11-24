ebcc2936022c10d65dae887ae32e8aa6b5e747e0
EatFan <eatfan0921@163.com>
2025-11-24 23:07:02 +0800

    feat(stater): 添加JunoYi框架版本配置与自定义Banner显示
    - 新增application.yml配置文件，定义框架版本号
    - 创建JunoProperties配置类，用于读取框架版本信息
    - 移除原有的关闭Banner设置，替换为自定义Banner实现
    - 自定义Banner中展示JunoYi框架版本和Spring Boot版本
    - Banner设计采用彩色ASCII艺术字样式增强视觉效果

--- junoyi-framework/junoyi-framework-stater/src/main/java/cn/junoyi/framework/stater/JunoYiApplication.java
+++ junoyi-framework/junoyi-framework-stater/src/main/java/cn/junoyi/framework/stater/JunoYiApplication.java
@@ -1,6 +1,5 @@
 package cn.junoyi.framework.stater;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 
 /**
@@ -17,7 +16,32 @@ public class JunoYiApplication {
      */
     public static void run(Class<?> primarySource, String[] args){
         SpringApplication springApplication = new SpringApplication(primarySource);
-        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.setBanner((environment, sourceClass, out) -> {
+            final String BLUE = "\033[38;5;39m";
+            final String WHITE = "\033[97m";
+            final String RESET = "\033[0m";
+
+            out.println(BLUE +
+                    " _____                              __    __       \n" +
+                    "/\\___ \\                            /\\ \\  /\\ \\__    \n" +
+                    "\\/__/\\ \\  __  __    ___     ___    \\ `\\`\\\\/'/\\_\\   \n" +
+                    "   _\\ \\ \\/\\ \\/\\ \\ /' _ `\\  / __`\\   `\\ `\\ /'\\/\\ \\  \n" +
+                    "  /\\ \\_\\ \\ \\ \\_\\ \\/\\ \\/\\ \\/\\ \\L\\ \\    `\\ \\ \\ \\ \\ \\ \n" +
+                    "  \\ \\____/\\ \\____/\\ \\_\\ \\_\\ \\____/      \\ \\_\\ \\ \\_\\\n" +
+                    "   \\/___/  \\/___/  \\/_/\\/_/\\/___/        \\/_/  \\/_/\n" +
+                    "                                                   \n" + RESET
+            );
+
+            String junoyiVersion = environment.getProperty("junoyi.version","unknown");
+            // 打印JunoYi版本
+            out.println(WHITE + "JunoYi Framework Version: "+ junoyiVersion + RESET);
+            // 打印SpringBoot版本
+            out.println(WHITE + "Spring Boot Version: " + SpringApplication.class.getPackage().getImplementationVersion() + RESET);
+
+            out.println(" ");
+            out.println(" ");
+
+        });
         springApplication.run(args);
     }
 }
