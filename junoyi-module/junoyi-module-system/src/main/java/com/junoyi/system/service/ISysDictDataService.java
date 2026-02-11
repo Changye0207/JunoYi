1ea3c8763674d37d719580928a6ee25b66f452bc
EatFan <eatfan0921@163.com>
2026-02-11 15:40:57 +0800

    feat(system): 添加系统字典数据和类型实体类及服务接口
    - 创建SysDictData实体类用于字典数据管理
    - 创建SysDictType实体类用于字典类型管理
    - 定义ISysDictDataService字典数据业务接口
    - 实现SysDictDataServiceImpl字典数据服务接口
    - 配置MyBatis-Plus注解支持数据库操作
    - 继承BaseEntity基础实体类提供通用字段

---
+++ junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/service/ISysDictDataService.java
@@ -0,0 +1,10 @@
+package com.junoyi.system.service;
+
+/**
+ * 字典数据业务接口类
+ *
+ * @author Fan
+ */
+public interface ISysDictDataService {
+
+}
---
+++ junoyi-module/junoyi-module-system/src/main/java/com/junoyi/system/service/impl/SysDictDataServiceImpl.java
@@ -0,0 +1,11 @@
+package com.junoyi.system.service.impl;
+
+import com.junoyi.system.service.ISysDictDataService;
+
+/**
+ * 系统字典业务数据接口实现类
+ *
+ * @author Fan
+ */
+public class SysDictDataServiceImpl implements ISysDictDataService {
+}
